9995ef48d81aaa7a5dc5e3dabc20b0c8ebec5581
Даша <56867929+Taramin@users.noreply.github.com>
2020-09-19 22:29:50 +0300

    Задача 2.
    Реализация задачи "Фибоначчи без рекурсии"

--- Program.cs
+++ Program.cs
@@ -1,25 +1,62 @@
 ﻿using System;
+using System.Threading;
 
-namespace SharpLearning
+namespace Fibonachi
 {
     class Program
     {
+        /*Функция нахождение чисел Фибоначчи, принимает, как аргумент, единственное целое число n - номер элемента
+          последовательности, который необходимо найти. Чи́сла Фибона́ччи — элементы числовой последовательности в которой 
+          первые два числа равны 0 и 1 (в данном случае), а каждое последующее число равно сумме двух предыдущих чисел.*/
+        static ulong fibonachi(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else if (n > 1)
+            {
+                int i = 0;
+                ulong result = 0, fib_1 = 1, fib_2 = 1;
+                while (i < n - 2)
+                {
+                    result = fib_1 + fib_2;
+                    fib_1 = fib_2;
+                    fib_2 = result;
+                    i++;
+                }
+                return result;
+            }
+            return 0;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number from 0 to 99");
-            int guessedNumber = Convert.ToInt32(Console.ReadLine());
-            int left = 0, right = 99, average;
-            while (true)
+            Console.WriteLine("Выберите действие:\n1 - нахождение числа Фибоначчи по номеру элемента\n2 - вывод ряда Фибоначчи на экран до n-ого значения");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
             {
-                average = (left + right) / 2;
-                if (guessedNumber < average) right = average - 1;
-                else if (guessedNumber > average) left = average + 1;
-                else
-                {
-                    Console.WriteLine("Guessed number: " + average);
-                    break;
-                }
+                case 1:
+                    {
+                        Console.WriteLine("Введите число: ");
+                        int input = Convert.ToInt32(Console.ReadLine());
+                        //обычный вывод, если вы не любите смайлики: Console.WriteLine("Число Фибоначчи для " + input + " = " + fibonachi(input));
+                        Console.WriteLine("число_    _    _"+fibonachi(input)+"\n      |_(o_o)_|\n    \t  |\n    \t _|_\n     \t|   |\n     \t|   |\nявляется-   - числом Фибоначчи для "+input);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Введите число элементов, которые хотите вывести: ");
+                        int input = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Ряд Фибоначчи для "+input+": ");
+                        for (int i = 0; i < input; i++)
+                        {
+                            Console.Write(fibonachi(i) + " ");
+                        }
+                        break;
+                    }
             }
+           
         }
     }
 }
