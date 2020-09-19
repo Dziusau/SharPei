3447930b5cf0905123e49571657ee8e2032bc65a
Dziusau <43704303+Dziusau@users.noreply.github.com>
2020-09-19 20:31:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SharpLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number from 0 to 99");
+            int guessedNumber = Convert.ToInt32(Console.ReadLine());
+            int left = 0, right = 99, average;
+            while (true)
+            {
+                average = (left + right) / 2;
+                if (guessedNumber < average) right = average - 1;
+                else if (guessedNumber > average) left = average + 1;
+                else
+                {
+                    Console.WriteLine("Guessed number: " + average);
+                    break;
+                }
+            }
+        }
+    }
+}
