22e58aa2ef44fe7b061b932f9d57d859b34cb8dc
Billy Milligan <sergeinovodvorsky@gmail.com>
2020-09-24 21:38:22 +0300

    Ya urod.

--- Task_1.cs
+++ Task_1.cs
@@ -8,21 +8,7 @@ namespace C_SHARP
         static void Main(string[] args)
         {
             Console.Write("Enter n: ");
-            var n = Convert.ToInt32(Console.ReadLine());
-            if (n < 1)
-            {
-                Console.WriteLine("Factorial is 1");
-                return;
-            }
 
-            var factorial = 1;
-            for (var i = 1; i < n + 1; ++i)
-            {
-                factorial = factorial * i;
-            }
-
-            Console.WriteLine($"Factorial is {factorial}");
-        }
     }
 
 }
