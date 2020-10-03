405b85185eca790b027125ea0b7067944a339106
Denis <khusainov02@icloud.com>
2020-10-03 17:00:58 +0300

    Tasks3: Created project.

---
+++ Sharpei/Tasks3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tasks3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
