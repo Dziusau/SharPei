6bb062f2d6447d32531cd9d70030f9a334970e0a
Denis <khusainov02@icloud.com>
2020-10-03 16:49:07 +0300

    Task1: Deleted file for Task 1 and moved code to the new project.

--- Sharpei/Task1/Program.cs
+++ Sharpei/Task1/Program.cs
@@ -6,7 +6,21 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter n: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+            if (n < 1)
+            {
+                Console.WriteLine("Factorial is 1");
+                return;
+            }
+
+            var factorial = 1;
+            for (var i = 1; i < n + 1; ++i)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"Factorial is {factorial}");
         }
     }
 }
