074eea1d233f11d6846efd945d90f6fa35338273
Taramin <56867929+Taramin@users.noreply.github.com>
2020-10-04 18:16:12 +0300

    Add files via upload

---
+++ Sharpei/Tasks3/Ducks/Ducks/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ducks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>();
+            ducks.Add(new Wild_duck());
+            ducks.Add(new Home_duck());
+            ducks.Add(new Gray_duck());
+            ducks.Add(new Black_duck());
+            ducks.Add(new Autistic_duck());
+            ducks.Add(new Rubber_duck());
+            ducks.Add(new Loving_duck());
+            ducks.Add(new Donald_duck());
+            ducks.Add(new Crazy_duck());
+            ducks.Add(new Strange_duck());
+
+            Random rand = new Random();
+            while (true)
+            {
+                Console.WriteLine("Нажмите что-нибудь, чтобы услышать уточку, 1 - выход");
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    return;
+                }
+                else 
+                {
+                    int number = rand.Next(0, 10);
+                    ducks[number].quack();
+                }
+            }
+        }
+    }
+
+    interface quackle
+    {
+        public void quack();
+    }
+
+    class Duck : quackle
+    {
+        public virtual void quack(){}
+    }
+
+    class Wild_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я хожу на пары в субботу!");
+        }
+    }
+
+    class Home_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я всегда сижу дома!");
+        }
+    }
+
+    class Gray_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я люблю хлеб и купаться!");
+        }
+    }
+
+    class Black_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я люблю рэп и играть в баскетбол!");
+        }
+    }
+
+    class Autistic_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Паскаль-АВС мой любимый язык программирования!");
+        }
+    }
+
+    class Rubber_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я люблю, когда со мной купаются!");
+        }
+    }
+
+    class Loving_duck : Duck
+    {
+        private String team;
+        private void find_favorite_team()
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 5);
+            if(number == 1)
+            {
+                team = "Ёжики";
+            }
+            else if (number == 2)
+            {
+                team = "Ретиколо";
+            }
+            else if (number == 3)
+            {
+                team = "Решеточки";
+            }
+            else if (number == 4)
+            {
+                team = "Акцио";
+            }
+            else if (number == 5)
+            {
+                team = "Ъеъ";
+            }
+        }
+        public override void quack()
+        {
+            find_favorite_team();
+            Console.WriteLine("Я люблю команду "+team);
+        }
+    }
+
+    class Donald_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Голосуйте за Трампа!");
+        }
+    }
+
+    class Crazy_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я использую goto!");
+        }
+    }
+
+    class Strange_duck : Duck
+    {
+        public override void quack()
+        {
+            Console.WriteLine("Я программирую на java!");
+        }
+    }
+}
