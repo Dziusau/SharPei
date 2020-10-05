4b6ff8b44c28fb9bb2078a7839bba3dffdb0eea1
Dziusau <43704303+Dziusau@users.noreply.github.com>
2020-10-05 16:28:49 +0300

    Add class Black_Box

---
+++ Black_Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Black_Box<T>
+{
+    private T variable;
+
+    public T Variable 
+    {
+        set 
+        {
+            variable = value;   
+        }
+        get 
+        {
+            return variable;
+        } 
+    }
+
+	public Black_Box(T value)
+    {
+        Variable = value;
+    }
+}
+
