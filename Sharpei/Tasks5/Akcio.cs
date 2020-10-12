62db4bad5b108790806d6108d5f7190333727d43
Denis <khusainov02@icloud.com>
2020-10-12 22:06:58 +0300

    Moved Teams code to the new project.

---
+++ Sharpei/Tasks5/Akcio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks5
+{
+    class Akcio : Program
+    {
+        public override void Object()
+        {
+            Console.WriteLine("No, we're not pedicures. We're polupedicures");
+        }
+    }
+}
---
+++ Sharpei/Tasks5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tasks5
+{
+    class Program : Object
+    {
+        public virtual void Object()
+        {
+            Console.WriteLine("loshki");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Akcio akcio = new Akcio();
+            program.Object();
+            akcio.Object();
+
+            Console.ReadLine();
+        }
+    }
+}
