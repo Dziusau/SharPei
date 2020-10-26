f36bad4bc7be2cd1865dee50a1600bfd04485dc0
Dziusau <43704303+Dziusau@users.noreply.github.com>
2020-10-26 20:11:35 +0300

    add task 6/2

---
+++ Sharpei/Tasks6/UniqueWords.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class UniqueWords
+{
+    public static List<string> GetUniqueWords(string text)
+    {
+        text.ToLower();
+        string[] words = text.Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+        var uniqueWords = new List<string>();
+        foreach (string word in words)
+        {
+            if (uniqueWords.Count == 0)
+            {
+                uniqueWords.Add(word);
+                continue;
+            }
+            var i = 0;
+            for (; i < uniqueWords.Count; i++)
+                if (word == uniqueWords[i]) break;
+            if (i == uniqueWords.Count) uniqueWords.Add(word);
+        }
+        return uniqueWords;
+    }
+}
+
+
+                        
+        
