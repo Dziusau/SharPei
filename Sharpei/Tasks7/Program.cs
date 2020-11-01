9b34c672024ff55652c4f96c1edba134934d9334
Dziusau <43704303+Dziusau@users.noreply.github.com>
2020-11-01 20:04:37 +0300

    Add project 7 & task Rainbow

---
+++ Sharpei/Tasks7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Pipes;
+
+namespace Tasks7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rainbow rainbow = new Rainbow();
+            Console.WriteLine("Rainbow");
+            foreach (var color in rainbow)
+            {
+                Console.WriteLine(color);
+            }
+        }
+        class Rainbow
+        {
+            string[] colors = { "red", "orange", "yellow", "green", "light blue", "dark blue", "purple" };
+            public IEnumerator<string> GetEnumerator()
+            {
+                return new ColorEnumerator(colors);
+            }
+        }
+        class ColorEnumerator : IEnumerator<string>
+        {
+            string[] colors;
+            int position = -1;
+
+            public ColorEnumerator(string[] colors)
+            {
+                this.colors = colors;
+            }
+            public string Current
+            {
+                get
+                {
+                    if (position == -1 || position >= colors.Length)
+                        throw new InvalidOperationException();
+                    return colors[position];
+                }
+            } 
+            object IEnumerator.Current => throw new NotImplementedException();
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if(position< colors.Length - 1)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+    }
+}
